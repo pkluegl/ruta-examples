db8f34c82a6af6431ed7460eec98a6385f53c017
pkluegl <pkluegl@gmail.com>
2016-09-30 09:16:28 +0200

    get example up and running

--- ruta-german-novel-with-dkpro/src/main/java/org/apache/uima/ruta/novel/RunMainRutaAnnotator.java
+++ ruta-german-novel-with-dkpro/src/main/java/org/apache/uima/ruta/novel/RunMainRutaAnnotator.java
@@ -18,9 +18,11 @@
  */
 package org.apache.uima.ruta.novel;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.uima.UIMAException;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
@@ -28,24 +30,22 @@ import org.apache.uima.fit.factory.JCasFactory;
 import org.apache.uima.fit.pipeline.SimplePipeline;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.ruta.novel.Name.PersName;
 import org.junit.Assert;
 import org.junit.Test;
 
-import org.apache.uima.RutagermannovelwithdkproMain.RutaMention;
-
-public class MainTest {
+public class RunMainRutaAnnotator {
 
 	@Test
 	public void test() throws IOException, UIMAException {
 
-//		JCas jcas = JCasFactory.createJCas();
-//		jcas.setDocumentText("This text is processed by Ruta.");
-//		AnalysisEngineDescription aed = AnalysisEngineFactory.createEngineDescriptionFromPath("target/generated-sources/ruta/descriptor/org/apache/uima/RutagermannovelwithdkproMainRutaAnnotator.xml");
-//		SimplePipeline.runPipeline(jcas, aed);
-//		
-//		Collection<RutaMention> select = JCasUtil.select(jcas, RutaMention.class);
-//		Assert.assertEquals(1, select.size());
-//		Assert.assertEquals("Ruta", select.iterator().next().getCoveredText());
+		JCas jcas = JCasFactory.createJCas();
+		jcas.setDocumentText(FileUtils.readFileToString(new File("input/sample.txt"), "UTF-8"));
+		AnalysisEngineDescription aed = AnalysisEngineFactory.createEngineDescriptionFromPath("target/generated-sources/ruta/descriptor/org/apache/uima/ruta/novel/MainRutaAnnotator.xml");
+		SimplePipeline.runPipeline(jcas, aed);
+		
+		Collection<PersName> select = JCasUtil.select(jcas, PersName.class);
+		Assert.assertFalse(select.isEmpty());
 	}
 
 }
